e87296cb8c1b6c41c8e57c6be9ceff1d9052d04a
ishan dahal <ishan.dahal88@gmail.com>
2026-03-09 20:39:48 +0000

    feat: wire GET /books to real database with dependency injection

--- bookshelf_api/routes.py
+++ bookshelf_api/routes.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from pathlib import Path
+
+from bookshelf_api.db import list_books
+
+
+DEFAULT_DB = Path.home() / ".bookshelf.db"
 
 app = FastAPI()
 
+def path_to_db():
+    return DEFAULT_DB
+
 @app.get("/books")
-def book_list():
-    # returning a dummy book
-    return [
-        {
-            "id": 1, 
-            "author": "test author", 
-            "title": "test title",
-        }
-    ]
+def book_list(path: Annotated[Path, Depends(path_to_db)]):
+    response = list_books(path)
+    return response
+
+
+
+
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,18 +1,34 @@
+from pathlib import Path
 
-from bookshelf_api.routes import app 
+from bookshelf_api.db import add_book, init_db
+from bookshelf_api.models import Book
+from bookshelf_api.routes import app, path_to_db
 from fastapi.testclient import TestClient
+
+DEFAULT_DB_TEST = ".bookshelf.db"
 
 client = TestClient(app)
 
-def test_book_list():
-    response = client.get("/books")
 
-    assert response.status_code == 200
-    assert response.json() == [
-        {
-            "id": 1, 
-            "author": "test author", 
-            "title": "test title",
-        }
-    ]
+def test_book_list(tmp_path: Path):
+    def path_to_testdb():
+        return tmp_path / DEFAULT_DB_TEST
 
+    db_path = path_to_testdb()
+    init_db(db_path)
+
+    app.dependency_overrides[path_to_db] = path_to_testdb
+
+    try:
+        book1 = Book(author="Daniel Kahneman", title="Thinking Fast and Slow")
+        add_book(db_path, book1)
+        book2 = Book(author="Anonymous", title="Bible")
+        add_book(db_path, book2)
+
+        response = client.get("/books")
+
+        assert response.status_code == 200
+        assert len(response.json()) == 2
+
+    finally:
+        app.dependency_overrides.clear()
